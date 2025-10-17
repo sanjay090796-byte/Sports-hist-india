603a45271d0f3d0e7aeb9b84e3ca98fac223ae58
sanjay090796-byte <sanjay090796@gmail.com>
2025-10-17 09:35:38 +0530

    Sport_hist_india_pwa

---
+++ sw.js
@@ -0,0 +1,10 @@
+const cacheName = "shi-cache-v1";
+const filesToCache = ["/", "/index.html", "/manifest.json", "/icon-192.png", "/icon-512.png"];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(filesToCache)));
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(caches.match(e.request).then((r) => r || fetch(e.request)));
+});

0d20d1327449be74ee95388d6a06b8e0009044e8
sanjay090796-byte <sanjay090796@gmail.com>
2025-10-21 14:42:19 +0530

    Script.js

--- script.js
+++ script.js
@@ -15,3 +15,28 @@ document.addEventListener('DOMContentLoaded', function(){
   const yearSpan = document.getElementById('year');
   yearSpan.textContent = new Date().getFullYear();
 });
+<script>
+const videoWrappers = document.querySelectorAll('.thumb-wrapper');
+
+videoWrappers.forEach(wrapper => {
+  const thumb = wrapper.querySelector('img');
+  const playBtn = wrapper.querySelector('.play-button');
+  const video = wrapper.querySelector('.hidden-video');
+
+  // Click on thumbnail
+  thumb.addEventListener('click', () => {
+    thumb.style.display = 'none';
+    playBtn.style.display = 'none';
+    video.style.display = 'block';
+    video.play();
+  });
+
+  // Click on play button
+  playBtn.addEventListener('click', () => {
+    thumb.style.display = 'none';
+    playBtn.style.display = 'none';
+    video.style.display = 'block';
+    video.play();
+  });
+});
+</script>
